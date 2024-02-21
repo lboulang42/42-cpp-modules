b301b37a9eaaf0687cc847c5887caad479a26103
lboulang <lboulang@student.42.fr>
2024-02-21 20:52:53 +0100

    finished ex00

--- cpp08/ex00/Includes/easyfind.hpp
+++ cpp08/ex00/Includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lboulang <lboulang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 18:44:05 by lboulang          #+#    #+#             */
-/*   Updated: 2024/02/19 19:43:14 by lboulang         ###   ########.fr       */
+/*   Updated: 2024/02/21 20:48:48 by lboulang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,68 +16,37 @@
 
 #include <iostream>
 #include <algorithm>
+/*
+Écrivez une fonction template easyfind acceptant un type T. Elle prend deux para-
+mètres. Le premier est de type T et le second un nombre entier.
+Partant du principe que T est un container d’entiers, cette fonction doit trouver la
+première occurrence du second paramètre dans le premier paramètre.
+Si aucune occurrence n’a été trouvée, vous pouvez soit jeter une exception, soit re-
+tourner une valeur d’erreur de votre choix. Si vous avez besoin d’inspiration, jetez un oeil
+au comportement des containers standards
+*/
+
+class NotFound : public std::exception {
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("[easyfind()] - n not found in container.");
+		}
+};
 
 template <typename T>
-int *easyfind(T &container, int n) {
-	T found = find(container.begin(), container.end(), n);
-	if (found == container.end())
-		return NULL;
-	return &found;
+int *easyfind(T &container, int n)
+{
+	typename T::iterator it;
+	it = find(container.begin(), container.end(), n);
+	if (it != container.end())
+		return &(*it);
+	throw NotFound();
 }
 
-// class Array
-// {
-// 	public:
-// 		/*==========CANONICAL==========*/
-// 		/*default constructor*/
-// 		Array() : _array(new T[0]), _size(0) {};
-// 		/*copy constructor*/
-// 		Array (Array const &src) : _array(new T[src._size]), _size(src._size) {
-// 			for (unsigned int i = 0; i < src._size; i++)
-// 				_array[i] = src._array[i];
-// 		};
-// 		/*operator = */
-// 		Array &operator=(Array const &src) {
-// 			if (this != &src) {
-// 				delete [] _array;
-// 				_array = new T[src._size];
-// 				_size = src._size;
-// 				for (unsigned int i = 0; i < src._size; i++)
-// 					_array[i] = src._array[i];
-// 			}
-// 			return *this;
-// 		};
-// 		/*destructor*/
-// 		~Array() { delete [] _array; };
 
-// 		/*==========OTHER CONSTRUCTORS==========*/
-// 		Array(unsigned int n) : _array(new T[n]), _size(n) {
-// 			for (unsigned int i = 0; i < n; i++)
-// 				_array[i] = T();
-// 		};
 
-// 		/*==========OTHER OPERATORS==========*/
-// 		T &operator[](unsigned int i) const {
-// 			if (i >= _size)
-// 				throw Array::OutOfRange();
-// 			return _array[i];
-// 		};
 
-// 		/*==========GETTERS==========*/
-// 		unsigned int size() const { return _size; };
-		
-// 		/*==========EXCEPTIONS==========*/
-// 		class OutOfRange : public std::exception {
-// 			public:
-// 				virtual const char* what() const throw()
-// 				{
-// 					return ("[Array] - Index Out Of Range");
-// 				}
-// 		};
 
-// 	private :
-// 		T *_array;
-// 		unsigned int _size;
-// };
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,5 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboulang <lboulang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 16:03:59 by lboulang          #+#    #+#             */
+/*   Updated: 2024/02/21 20:51:43 by lboulang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #include "easyfind.hpp"
+#include <vector>
+
 int main()
 {
+   
+    try
+    {
+        std::cout << "============================================" << std::endl;
+        std::cout << "CHECKING FOR A VALUE IN THE VECTOR : " << std::endl;
+        std::cout << "============================================" << std::endl;
+        std::vector<int> v;
+        for (int i = 0; i < 10; i++)
+        {
+            v.push_back(i * 4); // 0 4 8 12 16 20 24 28 32 36
+        }
+        std::cout << "vector[7] = " << v.at(7) << std::endl << std::endl;
+        
+        int *ptr = easyfind(v, 28);
+        std::cout << "Value pointed by ptr returned by easyfind : " << *ptr << std::endl << std::endl;
+        
+        std::cout << "changing the value pointed by ptr to 12 should change the value in the vector" << std::endl;
+        *ptr = 12;
+        std::cout << "vector[7] after change = " << v.at(7) << std::endl;
+        std::cout << "============================================" << std::endl;
+        
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    try
+    {
+        std::cout << "============================================" << std::endl;
+        std::cout << "CHECKING FOR A VALUE NOT IN THE VECTOR : " << std::endl;
+        std::cout << "============================================" << std::endl;
+        std::vector<int> v;
+        for (int i = 0; i < 10; i++)
+        {
+            v.push_back(i); // 0 1 2 3 4 5 6 7 8 9
+        }
+        int *ptr = easyfind(v, 12);
+        std::cout << "value pointed by ptr returned by easyfind : " << *ptr << std::endl;
+        
+    }
+    catch (std::exception &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+    
+    return 0;
 }
