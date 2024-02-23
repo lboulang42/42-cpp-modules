4de8f7d3a0d522609c15a2ca7635349f77b9c201
lboulang <lboulang@student.42.fr>
2024-02-23 16:06:15 +0100

    cpp08 ex02

---
+++ cpp08/ex02/Includes/MutantStack.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboulang <lboulang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 20:54:25 by lboulang          #+#    #+#             */
+/*   Updated: 2024/02/23 15:21:43 by lboulang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        /*==========CANONICAL==========*/
+        MutantStack() : std::stack<T>(){
+            std::cout << "MutantStack default constructor called" << std::endl;
+        }
+        MutantStack(MutantStack const &src) : std::stack<T>(src) {
+            std::cout << "MutantStack copy constructor called" << std::endl;    
+        }
+        MutantStack &operator=(MutantStack const &rhs) {
+            std::cout << "MutantStack assignation operator called" << std::endl;
+            std::stack<T>::operator=(rhs);
+            return (*this); 
+        }
+        ~MutantStack() {
+            std::cout << "MutantStack destructor called" << std::endl;
+        }
+
+        /*==========MEMBER FUNCTIONS==========*/
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        iterator begin() {
+            return (std::stack<T>::c.begin());
+        }
+        iterator end() {
+            return (std::stack<T>::c.end());
+        }
+    private:
+};
+
+#endif  
---
+++ cpp08/ex02/main.cpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboulang <lboulang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 15:06:12 by lboulang          #+#    #+#             */
+/*   Updated: 2024/02/23 16:05:06 by lboulang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <list>
+
+int	main(void)
+{
+	{
+		std::cout << "============================================" << std::endl;
+        std::cout << "MAIN FROM SUBJECT" << std::endl;
+        std::cout << "============================================" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	{
+		std::cout << "============================================" << std::endl;
+        std::cout << "MAIN FROM SUBJECT USING LIST" << std::endl;
+        std::cout << "============================================" << std::endl;
+		std::list<int> mstack_list;
+		mstack_list.push_back(5);
+		mstack_list.push_back(17);
+		std::cout << mstack_list.back() << std::endl;//equivqlent of .top()
+		mstack_list.pop_back();//equivalent of .pop()
+		std::cout << mstack_list.size() << std::endl;
+		mstack_list.push_back(3);
+		mstack_list.push_back(5);
+		mstack_list.push_back(737);
+		mstack_list.push_back(0);
+		std::list<int>::iterator it = mstack_list.begin();
+		std::list<int>::iterator ite = mstack_list.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> s(mstack_list);
+	}
+	return (0);
+}
